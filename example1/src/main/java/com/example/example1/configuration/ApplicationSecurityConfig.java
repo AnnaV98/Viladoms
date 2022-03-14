44f8fdea609eb4323fdef0be5c4c041ffd650bc4
Enric97 <enricvima97@gmail.com>
2022-03-14 11:34:10 +0100

    security correctly applied

--- example1/src/main/java/com/example/example1/configuration/ApplicationSecurityConfig.java
+++ example1/src/main/java/com/example/example1/configuration/ApplicationSecurityConfig.java
@@ -1,35 +1,48 @@
 package com.example.example1.configuration;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
+@Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.password}")
+    private String password;
 
     public ApplicationSecurityConfig() {}
 
+    //No serveix de res,
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
 
-                .antMatchers("/**").permitAll()
+                .antMatchers("/**").hasRole("ADMIN")
 
                 .anyRequest()
                 .authenticated()
 
+
                 .and()
+                .httpBasic().and()
 
                 .csrf().disable()
 
-                .headers().frameOptions().disable()
+                .headers().frameOptions().sameOrigin()
 
                 .and()
                 .cors().and()
@@ -37,4 +50,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        System.out.println("User:"+ user);
+        System.out.println("Password:"+password);
+        auth.inMemoryAuthentication()
+                .withUser(user).password(passwordEncoder().encode(password)).authorities("ROLE_ADMIN");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
